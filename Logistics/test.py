6a951bb6f7625542930fa5d27a21f793145331e4
KevinGnanaraj <kpg21@ic.ac.uk>
2025-02-03 11:25:21 +0000

    Initial Logistics Work
    Added files for initial work on Discord bot and Database

---
+++ Logistics/test.py
@@ -0,0 +1,66 @@
+# bot.py
+import os
+import random #testing
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+## Setup intents
+intents_def = discord.Intents.default()  # This enables the default intents
+intents_def.message_content = True
+
+client = discord.Client(intents=intents_def)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+## Create an event that happens when a user types \summon <seat_number
+
+
+
+client.run(TOKEN)
+
