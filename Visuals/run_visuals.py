7f6d0fdac24442ca891a86e55881d3ff2a32d314
MITeo21 <meigan.teo21@imperial.ac.uk>
2025-02-23 01:29:33 +0000

    More abstraction to prep for integration

---
+++ Visuals/run_visuals.py
@@ -0,0 +1,40 @@
+import pygame
+import character as char
+
+def visuals_initialisation():
+    pygame.init()
+    #pygame.mixer.init()
+    screen = pygame.display.set_mode((1920,1200)) # TODO: set screen size to fill
+    pygame.display.set_caption("Visual Interaction Endpoint")
+    character = char.Character(screen, "audio")
+    running = True
+
+    return screen, character, running
+
+def visuals_update_loop(screen, character):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        if event.type == pygame.MOUSEBUTTONDOWN: # DEBUG HERE
+            character.addAudio('test.mp3', "thinking")
+
+    character.update()
+
+    screen.fill((40,40,150))
+    character.draw()
+
+    pygame.display.flip()
+
+    return True
+
+def visuals_shutdown():
+    pygame.quit()
+
+if __name__ == "__main__":
+    visuals_screen, visuals_character, visuals_running = visuals_initialisation()
+    while visuals_running:
+        visuals_running = visuals_update_loop(visuals_screen, visuals_character)
+    visuals_shutdown()
+
+
+    
