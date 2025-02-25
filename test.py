1a37d6ceca17d9649efe9d50cb83f271866f9388
KevinGnanaraj <kpg21@ic.ac.uk>
2025-02-25 18:07:23 +0000

    Update test.py
    Testing LLM functionality with forklift control

--- test.py
+++ test.py
@@ -1,18 +1,38 @@
 from LLM.session import ChatSession
-from Logistics.databaseTest import create_database,insert_component,fetch_component
+from Logistics.databaseTest import SerialController
 
 
-def check_component_availability(name: str) -> str:
+# def check_component_availability(name: str) -> str:
+# '''
+# Determines the availability of an item in the lab, do not consider the item's relavence to robotics.
+
+# Args:
+# name: Name of the item required
+
+# Returns:
+# str: A string containing the quantity and location of the component if found
+# '''
+
+# return fetch_component(name)
+
+def requestBox(box_id : int) -> int:
+
     '''
-    Determines the availability of an item in the lab, do not consider the item's relavence to robotics.
+    Fetches information from user about the box they want to fetch, specifically the shelf number.
 
     Args:
-        name: Name of the item required
+    Box_ID : The ID of the box the user wants to fetch
 
     Returns:
-        str: A string containing the quantity and location of the component if found 
+    int : The shelf number of the box the user wants to fetch
     '''
+    comms = SerialController
 
-    return fetch_component(name)
-session = ChatSession([check_component_availability])
-session.query("Do we have any raspberry pis in the lab?")
+    return comms.user_box_fetch(box_id)
+
+# session = ChatSession([check_component_availability])
+# session.query("Do we have any raspberry pis in the lab?")
+
+
+session = ChatSession([requestBox])
+session.query("Can you fetch me a box 5")
